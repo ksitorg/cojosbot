53e98977ae715f0e5cb00bec832237424fccdad6
Tim <tsocialw@gmail.com>
2016-05-28 01:55:27 +0200

    - Initial

---
+++ app/Http/routes.php
@@ -0,0 +1,87 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+
+
+
+$prefix = '/cojosbot';
+
+Route::get($prefix.'/', function () {
+    return view('welcome');
+});
+
+
+
+// Example of POST Route:
+Route::post($prefix.'/'.'226204927:AAGI6oInXqlJKbQO2E5-KBODHgcl2lbEvhI'.'/webhook', function () {
+
+    Telegram::addCommands([
+        Telegram\Bot\Commands\HelpCommand::class,
+        App\BotCommands\StartCommand::class
+    ]);
+
+    $update = Telegram::commandsHandler(true);
+
+    // Commands handler method returns an Update object.
+    // So you can further process $update object
+    // to however you want.
+
+
+
+    $updates = Telegram::getWebhookUpdates();
+
+    return 'ok';
+});
+
+
+Route::get($prefix.'/'.'getme', function () {
+    $response = Telegram::getMe();
+
+    $botId = $response->getId();
+    $firstName = $response->getFirstName();
+    $username = $response->getUsername();
+
+    return view('vendor.getme');
+});
+
+Route::get($prefix.'/'.'sethook', function () {
+
+    // Or if you are supplying a self-signed-certificate
+    $response = Telegram::setWebhook([
+        'url' => 'https://31.18.208.109/cojosbot/'.'226204927:AAGI6oInXqlJKbQO2E5-KBODHgcl2lbEvhI'.'/webhook',
+        'certificate' => '/public/key.pub'
+    ]);
+
+    return $response->getHttpStatusCode().$response->getBody();
+});
+
+Route::get($prefix.'/'.'deletehook', function () {
+
+    // Or if you are supplying a self-signed-certificate
+    $response = Telegram::removeWebhook();
+
+    return $response->getBody();
+});
+
+Route::get($prefix.'/'.'updates', function () {
+
+    Telegram::addCommands([
+        Telegram\Bot\Commands\HelpCommand::class,
+        App\BotCommands\StartCommand::class,
+    ]);
+
+    $update = Telegram::commandsHandler(true);
+
+    $response = Telegram::getUpdates();
+    return $response;
+});
